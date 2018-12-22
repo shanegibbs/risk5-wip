36dfdefd1c476b4ff631347183fbd8cf06a3e6f2
Shane Gibbs <shane@hands.net.nz>
2018-12-21 21:29:49 -0600

    save

--- src/insns.rs
+++ src/insns.rs
@@ -174,7 +174,7 @@ pub fn sw<M: Memory>(p: &mut Processor<M>, rs1: usize, rs2: usize, imm: i32) {
 
 #[insn(kind=I,mask=0x110,match=0x100)]
 pub fn addi<M: Memory>(p: &mut Processor<M>, rd: usize, rs: usize, imm: u32) {
-    let v = p.regs.get(rs) as i64 + sign_extend(imm, 12);
+    let v = (p.regs.get(rs) as i64).wrapping_add(sign_extend(imm, 12));
     p.regs.set(rd, v as u64);
     p.advance_pc();
 }
