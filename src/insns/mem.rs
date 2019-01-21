063b4143a392c9e5d269004b9beece1d382f4332
Shane Gibbs <shane@hands.net.nz>
2019-01-20 21:42:09 -0600

    save

--- src/insns/mem.rs
+++ src/insns/mem.rs
@@ -31,6 +31,14 @@ pub fn lbu<M: Memory>(p: &mut Processor<M>, i: Itype) {
     p.advance_pc();
 }
 
+pub fn lhu<M: Memory>(p: &mut Processor<M>, i: Itype) {
+    let rs1 = p.regs.geti(i.rs1() as usize);
+    let addr = (rs1 + i.imm()) as u64;
+    let v = mem!(p, read_h, addr);
+    p.regs.set(i.rd() as usize, v as u64);
+    p.advance_pc();
+}
+
 pub fn lb<M: Memory>(p: &mut Processor<M>, i: Itype) {
     let rs1 = p.regs.geti(i.rs1() as usize);
     let v = mem!(p, read_b, (rs1 + i.imm()) as u64);
